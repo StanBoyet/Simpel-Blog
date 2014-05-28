bef7615ba4b2426cd70e3bd3a359598b7a938854
Stanislas BOYET <stanislas@boyet.me>
2014-05-29 00:29:15 +0200

    add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :types
+	resources :types
 
-  resources :posts
+	resources :posts
 
-  root :to => "posts#index"
-  devise_for :users, :controllers => {omniauth_callbacks: 'users/omniauth_callbacks' }
+	match '/users/:id', :to => 'users#show',    :as => :user,         :via => :get
+
+	root :to => "posts#index"
+	devise_for :users, :controllers => {omniauth_callbacks: 'users/omniauth_callbacks' }
 end
